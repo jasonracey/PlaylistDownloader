10c7f66f45a0932a7de7080488060a043db72968
Jason Racey <jasonracey@gmail.com>
2022-04-04 21:41:07 -0700

    Fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
-dotnet build -c Release PlaylistDownloader.sln
-rm -rf ~/Files/Software/Tools/PlaylistDownloader
-mkdir ~/Files/Software/Tools/PlaylistDownloader
-cp -r ./PlaylistDownloaderConsole/bin/Release/net6.0/ ~/Files/Software/Tools/PlaylistDownloader
+# deploy console
+echo "Deploying Console..."
+rm -Rf ../../Tools/PlaylistDownloader
+mkdir ../../Tools/PlaylistDownloader
+cp -R ./PlaylistDownloaderConsole/bin/Release/netstandard2.1/ ../../Tools/PlaylistDownloader
 
+# deploy app
+echo "Deploying UI..."
+rm -Rf /Applications/Playlist\ Downloader.app
+cp -Rf ./PlaylistDownloaderUI/bin/Release/Playlist\ Downloader.app /Applications/Playlist\ Downloader.app
+
+echo "Done!"
